431dbcdd685ee73a4a4becf875c869a59e7e4b33
Tanishka Ruhela <tanishkaruhela@metasquaretech.com>
2026-06-25 09:42:22 +0530

    HabitFlow frontend ready

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://habit-tracker-mern-final.onrender.com/',
         changeOrigin: true,
       }
     }
